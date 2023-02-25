a0d4902637b05ce0ef8b80d0e8c0b7dd7a9bc8f1
Tasin Mohammad <tasin.mohammad@g.bracu.ac.bd>
2023-02-25 22:37:05 +0600

    Version 3

--- tut1.py
+++ tut1.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
 
 
-@app.route("/tasin")
-def tasin():
-    return "<p>Hello, Tasin! How you doin!</p>"
+@app.route("/about")
+def about():
+    name = 'Tasin'
+    return render_template('about.html', name_temp=name)
 
 
 app.run(debug=True)
