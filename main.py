0ed9d0ec204c2b8ab2a4fc571cb340cc2717f73d
Tasin Mohammad <tasinms.bd@gmail.com>
2023-03-09 01:56:59 +0600

    Version 4

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def home():
     return render_template('index.html')
+
+
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
 
 # @app.route("/about")
