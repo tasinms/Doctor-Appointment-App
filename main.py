09726445a5c31890ee3093f698ee7fd30ba48e70
Tasin Mohammad <tasinms.bd@gmail.com>
2023-03-14 12:41:55 +0600

    Version 6

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import json
 
+local_server = True
 with open('config.json', 'r') as c:
     parameters = json.load(c)['parameters']
+
 db = SQLAlchemy()
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/doccure"
+
+if local_server:
+    app.config["SQLALCHEMY_DATABASE_URI"] = parameters['local_uri']
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = parameters['host_uri']
+
 db.init_app(app)
 
 
@@ -18,9 +25,24 @@ class Patients(db.Model):
     password = db.Column(db.String(30), unique=False, nullable=False)
 
 
+class Doctors(db.Model):
+    doctor_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=False, nullable=False)
+    doctor_image = db.Column(db.String(20), unique=False, nullable=True)
+    doctor_thumb_image = db.Column(db.String(20), unique=False, nullable=True)
+    gender = db.Column(db.String(20), unique=False, nullable=False)
+    speciality = db.Column(db.String(30), unique=False, nullable=False)
+    department = db.Column(db.String(30), unique=False, nullable=False)
+    location = db.Column(db.String(30), unique=False, nullable=False)
+    overview = db.Column(db.String(30), unique=False, nullable=False)
+    fee = db.Column(db.Integer, unique=False, nullable=False)
+    slug = db.Column(db.String(30), unique=True, nullable=False)
+
+
 @app.route("/")
 def home():
-    return render_template('index.html')
+    doctors = Doctors.query.filter_by().all()
+    return render_template('index.html', parameters=parameters, doctors=doctors)
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -39,12 +61,18 @@ def register():
         db.session.add(entry)
         db.session.commit()
 
-    return render_template('register.html')
+    return render_template('register.html', parameters=parameters)
+
+
+@app.route("/doctor-profile/<string:doctor_profile_slug>", methods=['GET'])
+def doctor_profile(doctor_profile_slug):
+    doctor = Doctors.query.filter_by(slug=doctor_profile_slug).first()
+    return render_template('doctor-profile.html', parameters=parameters, doctor=doctor)
 
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    return render_template('login.html', parameters=parameters)
 
 
 # @app.route("/about")
